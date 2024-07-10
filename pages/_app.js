0aee4d76038b992a03c12020bb3561e7e0564983
dani <bonmobp@gmail.com>
2024-07-10 22:32:30 +0300

    remove audio

--- pages/_app.js
+++ pages/_app.js
@@ -1,66 +1,27 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Head from "next/head";
 import dynamic from "next/dynamic";
-import "../styles/index.css";
-import "../styles/planets.scss";
-import "../styles/header.scss";
-import "../styles/footer.css";
+import "../styles/main.scss";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
-import Loading from "./loading"; 
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import Loading from "./loading";
+import { Analytics } from "@vercel/analytics/react";
 
-const StarsCanvas = dynamic(() => import("./background"), { ssr: false });
-const AudioButton = dynamic(() => import("./header"), { ssr: false });
+const StarsCanvas = dynamic(() => import("./background.tsx"), { ssr: false });
 const Footer = dynamic(() => import("./Footer.js"), { ssr: false });
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically
 
 function MyApp({ Component, pageProps }) {
-    const audioRef = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(true);
     const [loading, setLoading] = useState(true);
     const router = useRouter();
-
-    const handlePlayPause = () => {
-        if (audioRef.current) {
-            if (isPlaying) {
-                audioRef.current.pause();
-            } else {
-                audioRef.current.play();
-            }
-            setIsPlaying(!isPlaying);
-        }
-    };
-
-    useEffect(() => {
-        const startAudio = () => {
-            if (audioRef.current) {
-                audioRef.current.play().catch(error => {
-                    // Autoplay was prevented, let's start the audio when the user interacts with the page
-                    document.addEventListener("click", startAudio);
-                });
-                document.removeEventListener("click", startAudio);
-            }
-        };
-
-        document.addEventListener("click", startAudio);
-        return () => {
-            document.removeEventListener("click", startAudio);
-        };
-    }, []);
 
     useEffect(() => {
         const imagePaths = [
             "/img/earth.gif",
-
             "/img/venus.gif",
-
             "/img/mercury.gif",
-
-            "/img/blackhole.webm",
-
-            "/sound.m4a"
+            "/img/blackhole.webm"
         ];
 
         let loadedItemsCount = 0;
@@ -71,15 +32,6 @@ function MyApp({ Component, pageProps }) {
                 video.src = path;
                 video.preload = "metadata";
                 video.onloadeddata = () => {
-                    loadedItemsCount++;
-                    if (loadedItemsCount === imagePaths.length) {
-                        setLoading(false);
-                    }
-                };
-            } else if (path.endsWith(".m4a")) {
-                const audio = new Audio(path);
-                audio.preload = "auto";
-                audio.oncanplaythrough = () => {
                     loadedItemsCount++;
                     if (loadedItemsCount === imagePaths.length) {
                         setLoading(false);
@@ -101,36 +53,14 @@ function MyApp({ Component, pageProps }) {
 
     return (
         <>
-            <Head>
-                <link rel="preload" href="/img/dani.png" as="image" />
-                <link rel="preload" href="/img/daniStars.png" as="image" />
-                <link rel="preload" href="/img/jounblat.png" as="image" />
-                <link rel="preload" href="/img/jounblatStars.png" as="image" />
-
-                <link rel="preload" href="/img/ss1.jpg" as="image" />
-                <link rel="preload" href="/img/fullsun.gif" as="image" />
-                <link rel="preload" href="/img/earth.gif" as="image" />
-                <link rel="preload" href="/img/satellite.gif" as="image" />
-                <link rel="preload" href="/img/venus.gif" as="image" />
-                <link rel="preload" href="/img/shuttle.gif" as="image" />
-                <link rel="preload" href="/img/mercury.gif" as="image" />
-                <link rel="preload" href="/img/ufo.gif" as="image" />
-                <link rel="preload" href="/img/blackhole.webm" as="video" />
-                <link rel="preload" href="/sound.m4a" as="audio" />
-                <link rel="preload" href="/img/loading.gif" as="image" />
-            </Head>
             {loading ? (
                 <Loading />
             ) : (
                 <>
-                    <AudioButton
-                        handlePlayPause={handlePlayPause}
-                        isPlaying={isPlaying}
-                    />
                     <StarsCanvas />
                     <Component {...pageProps} />
-                    <audio ref={audioRef} src="./sound.m4a" loop />
                     <Footer />
+                    <Analytics />
                 </>
             )}
         </>
