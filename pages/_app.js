67638f0be63f7b1ee5cb21b5bb52aefea8b5c163
dani <bonmobp@gmail.com>
2024-07-10 23:49:11 +0300

    gg

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import "../styles/main.scss";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Loading from "./loading";
 import { Analytics } from "@vercel/analytics/react";
 
 const StarsCanvas = dynamic(() => import("./background.tsx"), { ssr: false });
@@ -13,51 +12,30 @@ const Footer = dynamic(() => import("./Footer.js"), { ssr: false });
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically
 
 function MyApp({ Component, pageProps }) {
-    const [loading, setLoading] = useState(true);
     const router = useRouter();
 
     useEffect(() => {
         const imagePaths = [""];
-
-        let loadedItemsCount = 0;
 
         imagePaths.forEach(path => {
             if (path.endsWith(".webm")) {
                 const video = document.createElement("video");
                 video.src = path;
                 video.preload = "metadata";
-                video.onloadeddata = () => {
-                    loadedItemsCount++;
-                    if (loadedItemsCount === imagePaths.length) {
-                        setLoading(false);
-                    }
-                };
             } else {
                 const img = new Image();
                 img.src = path;
                 img.loading = "lazy";
-                img.onload = () => {
-                    loadedItemsCount++;
-                    if (loadedItemsCount === imagePaths.length) {
-                        setLoading(false);
-                    }
-                };
             }
         });
     }, []);
 
     return (
         <>
-            {loading ? (
-                <Loading />
-            ) : (
-                <>
-                    <StarsCanvas />
-                    <Component {...pageProps} />
-                    <Footer />
-                    <Analytics />
-                </>
-            )}
+            <StarsCanvas />
+            <Component {...pageProps} />
+            <Footer />
+            <Analytics />
         </>
     );
 }
